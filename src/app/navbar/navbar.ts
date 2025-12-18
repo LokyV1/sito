41bd3df804ce2757250a93b67a4c65d630691528
NovaTrigger <83513680+LokyV1@users.noreply.github.com>
2025-12-18 15:02:36 +0100

    feat: add Navbar component with Angular Material and custom theming

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatToolbarModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
