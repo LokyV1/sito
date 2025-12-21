728cf1aa1b7fe1aa36662900748a9c660f17cfe4
NovaTrigger <83513680+LokyV1@users.noreply.github.com>
2025-12-21 19:11:12 +0100

    aggiunta una api call con signal

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { signal } from '@angular/core';
 
 @Component({
   selector: 'app-main',
+  standalone: true,
   imports: [],
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
-export class Main {
+export class Main implements OnInit {
+  private http = inject(HttpClient);
 
+  //creazione della variabile signal
+  data = signal<string>('Caricamento...');
+
+  //attesa dei dati
+  async ngOnInit(): Promise<void> {
+    const response = await firstValueFrom(this.http.get<any>('/api/users'));
+    console.log(response);
+
+    //salva i dati nella variabile
+    this.data.set(response.data[0].first_name);
+  }
 }
